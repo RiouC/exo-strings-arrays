f018962da186c03d17675434609c5e4a9737b73f
RiouC <riou.christophe@protonmail.com>
2021-02-17 19:25:10 +0100

    decypher : add keys array and decipher function

--- decypher.js
+++ decypher.js
@@ -1,31 +1,57 @@
 const cipherText = 'YRMV PFL VMVI YRU R UIVRD EVF KYRK PFL NVIV JF JLIV NRJ IVRC NYRK ZW PFL NVIV LERSCV KF NRBV WIFD KYRK UIVRD YFN NFLCU PFL BEFN KYV UZWWVIVETV SVKNVVE KYV UIVRD NFICU REU KYV IVRC NFICU';
 
-// Etaoin shrdlu
-// https://en.wikipedia.org/wiki/Etaoin_shrdlu
 
-let cipher = cipherText.replaceAll(' ', '');
-let cipherSplit = cipher.split('');
+let cipher = cipherText.replaceAll(' ', '').split('');
 let alphabet = Array(26).fill(0);
 
-function decalage(c, n) {
+function encryptChar(c, n) {
     return (((c.charCodeAt(0) - 65) + n) % 26) + 65 ;
 }
 
 function countLetters(tab) {
-    for (letter of cipherSplit) {
+    for (letter of cipher) {
 	alphabet[letter.charCodeAt()-65] += 1;
     }
 }
 
-// console.log(cipherSplit.filter(x => x === 'e').length);
-
+// Etaoin shrdlu
+// https://en.wikipedia.org/wiki/Etaoin_shrdlu
+let etaoinshrdlu = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R', 'D', 'L', 'U'];
 countLetters(alphabet);
-console.log(`alphabet = ${alphabet}`);
-let tmp = alphabet.indexOf(Math.max(...alphabet)) + 1
-console.log(tmp);
-console.log(String.fromCharCode(tmp + 65));
-console.log(`cipherSplit = ${cipherSplit}`);
-console.log(`decalage('Z', 3) = ${decalage('Z', 3)}`);
+console.log(`Frequency : ${alphabet}`);
+
+let freqLetterCode = alphabet.indexOf(Math.max(...alphabet));
+console.log(`Most frequent letter in cypher text : ${String.fromCharCode(freqLetterCode + 65)}`);
+
+let keys = Array(etaoinshrdlu.length).fill(0);
+for (let i = 0; i < etaoinshrdlu.length; i++) {
+    keys[i] = freqLetterCode - (etaoinshrdlu[i].charCodeAt(0) - 65);
+}
+console.log(`Probable keys = ${keys}`);
+
+// function decypher(tab, key) {
+//     tab.map(function(letter) {
+// 	console.log(String.fromCharCode(encryptChar(letter, key)));
+//     })
+// }
+
+function decypher(tab, key) {
+    let res = '';
+	for (let i = 0; i < tab.length; i++) {
+	    res += String.fromCharCode(encryptChar(tab[i], 26 - key));
+	}
+    return res;
+}
+
+console.log(`Deciphered : ${decypher(cipher, keys[0])}`);
 
 // 'HELLO'.charCodeAt(0)
 // String.fromCharCode(72)
+
+// lines = lines.map(function(line) {
+//     return line.split(' ').length;
+// })
+
+// upperWords = sentence.map(function(word) {
+//     console.log(`mot: ${word.toUpperCase()}`);
+// })
