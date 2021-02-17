bf0945ca9850715f8cb723bcaadd7a26b261b83e
RiouC <riou.christophe@protonmail.com>
2021-02-17 14:54:50 +0100

    check : add check.js

---
+++ check.js
@@ -0,0 +1,6 @@
+
+let sentence = 'JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript';
+let word = 'Javascript';
+
+console.log(sentence.includes(word));
+console.log(sentence.indexOf(word) >= 0);
